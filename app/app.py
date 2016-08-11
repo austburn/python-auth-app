bd50ee1c388928e799f7ba297b7caab59ab31b09
Austin Burnett <austburn@gmail.com>
2016-08-11 13:59:03 -0500

    add form

--- app/app.py
+++ app/app.py
@@ -1,15 +1,20 @@
 from sqlalchemy import create_engine
 from schemas import Base
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 engine = create_engine('postgresql://austburn:pass1234@postgres', echo=True)
 Base.metadata.create_all(engine)
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return 'ok', 200
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def login():
+    print(request.form)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
