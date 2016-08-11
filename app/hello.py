b79923cb02c0763b96006479e656159323e4c350
Austin Burnett <austburn@gmail.com>
2016-08-11 12:58:29 -0500

    Actually connect to the db... lol

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://austburn:pass1234@postgres')
+engine = create_engine('postgresql://austburn:pass1234@postgres', echo=True)
+connection = engine.connect()
 
-print engine.name
+print connection.info
+
+connection.close()
