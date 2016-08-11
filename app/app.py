2a2790aa9d54cea6c5659cdbd37a3a706cbd5850
Austin Burnett <austburn@gmail.com>
2016-08-11 15:12:17 -0500

    Simple form post into the db

--- app/app.py
+++ app/app.py
@@ -1,11 +1,23 @@
 from sqlalchemy import create_engine
-from schemas import Base
-from flask import Flask, render_template, request, redirect
+from sqlalchemy.orm import sessionmaker
+from schemas import Base, User
+from flask import Flask, render_template, request, redirect, g
 
 engine = create_engine('postgresql://austburn:pass1234@postgres', echo=True)
 Base.metadata.create_all(engine)
 
 app = Flask(__name__)
+
+@app.before_request
+def initialize_session():
+    Session = sessionmaker(bind=engine)
+    g.session = Session()
+
+@app.after_request
+def setup_db(res):
+    g.session.commit()
+    g.session.close()
+    return res
 
 @app.route('/', methods=['GET'])
 def home():
@@ -13,8 +25,13 @@ def home():
 
 @app.route('/', methods=['POST'])
 def login():
-    print(request.form)
+    g.session.add(User(email=request.form['email'], password=request.form['password']))
     return redirect('/')
+
+@app.route('/users')
+def users():
+    users = g.session.query(User).all()
+    return ','.join([ u.email for u in users])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
