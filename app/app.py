d70dce0742cd0b302bc808ff0583c2a944b59a83
Austin Burnett <austburn@gmail.com>
2016-08-15 12:18:30 -0500

    Add more err handling

--- app/app.py
+++ app/app.py
@@ -26,10 +26,15 @@ def home():
 
 @app.route('/', methods=['POST'])
 def login():
-    user = g.session.query(User).filter(User.email==request.form['email']).one()
+    err_response = render_template('home.html', errors=['Looks like the email or password were incorrect.'])
+    try:
+        user = g.session.query(User).filter(User.email==request.form['email']).one()
+    except Exception:
+        return err_response
+
     if bcrypt.checkpw(bytes(request.form['password']), bytes(user.password)):
         return redirect('/success')
-    return redirect('/')
+    return err_response
 
 @app.route('/success', methods=['GET'])
 def success():
